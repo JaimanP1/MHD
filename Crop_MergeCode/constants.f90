75f80c107824d1887f68dae4f509b1853562a43e
Jaiman Parekh (hpc account) <jdp46@njit.edu>
2024-06-04 15:56:39 -0400

    more timesteps

--- Crop_MergeCode/constants.f90
+++ Crop_MergeCode/constants.f90
@@ -59,9 +59,9 @@
   complex(DPC), parameter :: IUNIT = (0.0_DP, 1.0_DP)
 
 ! === READ & OUTPUT FOLDER ============
-  character(*) ,parameter :: dir_r = '/project/wangj/node819/Jaiman/Wulver/sp24/ram_4_24/test3/DATA/'
-  character(*) ,parameter :: dir   = '/project/wangj/node819/Jaiman/Wulver/sp24/ram_4_24/test3/VAPOR2/merge/'
-  character(*) ,parameter :: dir_a = '/project/wangj/node819/Jaiman/Wulver/sp24/ram_4_24/test3/VAPOR2/'
+  character(*) ,parameter :: dir_r = '/project/wangj/node819/Jaiman/Wulver/sp24/ram_4_24/test4/DATA/'
+  character(*) ,parameter :: dir   = '/project/wangj/node819/Jaiman/Wulver/sp24/ram_4_24/test4/VAPOR/Merge/'
+  character(*) ,parameter :: dir_a = '/project/wangj/node819/Jaiman/Wulver/sp24/ram_4_24/test4/VAPOR/'
 
 ! === I/O file number
   integer, parameter :: FILE_SYSOUT       = 06 ! sysout file
