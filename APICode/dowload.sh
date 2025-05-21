1b39826efa3a217912be59f8be151e6b7be0b363
Jaiman Parekh (hpc account) <jdp46@njit.edu>
2025-05-21 10:57:10 -0400

    local data download and use in case API is weird

---
+++ APICode/dowload.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# If you want to download and use the data locally instead of using the API. Adjust ts and file_num parameters as needed
+
+# Output file
+output_file="commands.txt"
+> "$output_file"  # Clear the file if it exists
+
+# List of variables
+vars=("BX" "BY" "BZ" "CB2" "CT_BT" "VX" "VY" "VZ")
+
+# Loop through variables
+for var in "${vars[@]}"; do
+    for ((ts=0; ts<=30; ts++)); do
+        file_num=$(printf "%03d" $((ts + 50)))
+        echo "raw2vdc -ts $ts -varname ${var,,} test.vdc B3D.001.${var}.R.${file_num}" >> "$output_file"
+    done
+done
+
